e1192e0048cf28cb38c0aa23c698df5f9d4d6c31
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-09-24 18:23:01 +0530

    Problem: Implement Thread-safe Singleton (Double-checked locking)

---
+++ wearl/src/ThreadSafeSingletonDemo.java
@@ -0,0 +1,35 @@
+class ThreadSafeSingleton {
+    private static volatile ThreadSafeSingleton instance;
+
+    private ThreadSafeSingleton() {}
+
+    public static ThreadSafeSingleton getInstance() {
+        if (instance == null) {
+            synchronized (ThreadSafeSingleton.class) {
+                if (instance == null) {
+                    instance = new ThreadSafeSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
+
+
+public class ThreadSafeSingletonDemo {
+    public static void main(String[] args) {
+        Runnable task = () -> {
+            ThreadSafeSingleton s = ThreadSafeSingleton.getInstance();
+            System.out.println(Thread.currentThread().getName() + " -> " + s);
+        };
+
+        Thread t1 = new Thread(task, "Thread-1");
+        Thread t2 = new Thread(task, "Thread-2");
+        Thread t3 = new Thread(task, "Thread-3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
+
